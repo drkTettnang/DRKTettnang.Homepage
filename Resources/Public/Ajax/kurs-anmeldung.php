e645a25c5445f4c461180a48a9116a02d0407138
Klaus <klaus@jsxc.org>
2016-02-08 12:29:56 +0100

    fix encoding

--- Resources/Public/Ajax/kurs-anmeldung.php
+++ Resources/Public/Ajax/kurs-anmeldung.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+header('Content-Type: text/html; charset=ISO-8859-1');
+
 $query = parse_url($_SERVER[REQUEST_URI], PHP_URL_QUERY);
 
 echo file_get_contents('https://www.kurs-anmeldung.de/go.dll?'.$query);
