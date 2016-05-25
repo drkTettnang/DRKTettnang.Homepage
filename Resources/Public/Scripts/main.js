9acdac360b8a3f97dc80911709280597848c9571
Klaus <klaus@jsxc.org>
2016-05-25 15:44:13 +0200

    change socialshare
    - change fb like to fb share
    - add twitter

--- Resources/Public/Scripts/main.js
+++ Resources/Public/Scripts/main.js
@@ -650,27 +650,17 @@ if (drk && drk.config && drk.config.piwikUrl) {
    })();
 }
 
-var socialUri;
-
-if (drk && drk.config && drk.config.facebookUrl) {
-   socialUri = function() {
-      return drk.config.facebookUrl;
-   };
-}
-
 $('#socialmedia').socialSharePrivacy({
    'css_path': '',
    'lang_path': '/_Resources/Static/Packages/DRKTettnang.Homepage/Vendor/socialshareprivacy/lang/',
    'language': 'de',
-   'uri': socialUri,
    services: {
       facebook: {
-         'action': 'like',
          'perma_option': 'off',
-         'dummy_img': '/_Resources/Static/Packages/DRKTettnang.Homepage/Vendor/socialshareprivacy/images/dummy_facebook_like.png'
+         'dummy_img': '/_Resources/Static/Packages/DRKTettnang.Homepage/Vendor/socialshareprivacy/images/dummy_facebook_share_de.png'
       },
       twitter: {
-         'status': 'off',
+         'status': 'on',
          'perma_option': 'off',
          'dummy_img': '/_Resources/Static/Packages/DRKTettnang.Homepage/Vendor/socialshareprivacy/images/dummy_twitter.png'
       },
